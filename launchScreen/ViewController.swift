3ed2487090f0ffed5b9d05fe4d3c5858892d37a8
JunKoding <wnsrb0407@daum.net>
2022-04-17 19:55:36 +0900

    2022.04.17 - launchscreen 연습

---
+++ launchScreen/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  launchScreen
+//
+//  Created by JunKyu Lee on 2022/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        view.backgroundColor = .red
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
